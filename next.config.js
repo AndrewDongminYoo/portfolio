f6f04f138346f890a3d7bd182806a46507838d3e
Dongmin,Yoo <ydm2790@gmail.com>
2022-12-20 15:56:57 +0900

    🚀 NextJS Deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        loader: "imgix",
+        path: 'andrewdongminyoo.imgix.net'
+    }
 }
 
 module.exports = nextConfig
