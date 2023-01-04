b20010a230aa5f59c2f2ce537732dda4a5fa9152
Dongmin,Yoo <ydm2790@gmail.com>
2023-01-04 17:19:43 +0900

    🔧 Define Robots.txt and Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,34 @@
-/** @type {import('next-sitemap').IConfig} */
+/**
+ * @type {import('next-sitemap').IConfig}
+ * @property {string} siteUrl
+ * @property {number} sitemapSize
+ * @property {boolean} generateRobotsTxt
+ * @property {import('next-sitemap').IRobotPolicy} robotsTxtOptions
+ */
 module.exports = {
     siteUrl: 'https://AndrewDongminYoo.github.io',
-    generateRobotsTxt: true,
     sitemapSize: 5000,
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        includeNonIndexSitemaps: true,
+        policies: [{
+            userAgent: '*',
+            allow: [
+                '/repos',
+                '/',
+            ],
+            disallow: [
+                '/_next/static/chunks/pages/posts',
+                '/_next/static/chunks/pages/repos',
+                '/_next/static/chunks/pages',
+                '/_next/static/chunks',
+                '/_next/static/media',
+                '/_next/static/css',
+                '/_next/static/',
+                '/posts',
+                '/styles',
+            ],
+        }]
+    },
+
 }
