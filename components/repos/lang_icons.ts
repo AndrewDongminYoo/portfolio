498abe2f437e5d8b34443201c12ffa07b26e0905
Dongmin,Yoo <ydm2790@gmail.com>
2023-01-09 22:54:18 +0900

    🍱 Change asset icons to svg

---
+++ components/repos/lang_icons.ts
@@ -0,0 +1,24 @@
+import aws from "@public/images/stacks/aws-lambda.svg";
+import expo from "@public/images/stacks/expo.svg";
+import flask from "@public/images/stacks/flask.svg";
+import flutter from "@public/images/stacks/flutter.svg";
+import nextjs from "@public/images/stacks/nextjs.svg";
+import selenium from "@public/images/stacks/selenium.svg";
+import spring from "@public/images/stacks/spring.svg";
+import swagger from "@public/images/stacks/swagger.svg";
+import typescript from "@public/images/stacks/typescript-icon.svg";
+import webpack from "@public/images/stacks/webpack.svg";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const frameworks: Record<string, any> = {
+    "52market.front": webpack,
+    "52market.shop": swagger,
+    "AndrewDongminYoo.github.io": nextjs,
+    "do-it-react-native-clone": typescript,
+    "flask-My-Small-Meal": flask,
+    "flutter-proxy-voting-app": flutter,
+    "my-first-react-native-chatting-app": expo,
+    "Serverless-Framework-Crawlers": aws,
+    "TIL-of-BootCamp-Flask": selenium,
+    "TIL-of-BootCamp-Spring": spring,
+};
