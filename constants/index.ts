42524e1c9aea0a756935db4b844d964ea3ea7f6f
Dongmin,Yoo <ydm2790@gmail.com>
2023-01-09 15:07:26 +0900

    🍱 add image assets

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
-import email from '@public/images/contact_email.svg';
-import github from '@public/images/contact_github.svg';
-import phone from '@public/images/contact_phone.svg';
-import youtube from '@public/images/contact_youtube.svg';
+import email from '@public/images/contacts/email.svg';
+import github from '@public/images/contacts/github.svg';
+import phone from '@public/images/contacts/phone.svg';
+import youtube from '@public/images/contacts/youtube.svg';
 
 const username = 'AndrewDongminYoo';
 const myName = 'Andrew Dong-min, Yoo';
