a199efaf2c3aa4ee3bd52764307efed042d713ec
Dongmin,Yoo <ydm2790@gmail.com>
2023-01-15 08:47:04 +0900

    🚑 image config: WIP

--- next.config.js
+++ next.config.js
@@ -22,11 +22,11 @@ const nextConfig = {
         formats: ["image/webp", "image/avif"],
         minimumCacheTTL: 60,
         disableStaticImages: false,
-        path: isDevelopment ? "http://localhost:3000" : "https://andrewdongminyoo.imgix.net",
+        path: "https://andrewdongminyoo.imgix.net",
         loader: "imgix",
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        unoptimized: isDevelopment,
+        unoptimized: true,
     },
     async headers() {
         return [
