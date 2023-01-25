79c002d3d11babf1c73e86fca20208dd1299bcd9
Dongmin,Yoo <ydm2790@gmail.com>
2023-01-26 03:26:50 +0900

    🎨 some components function to class (to study)

--- pages/repos/[repo].tsx
+++ pages/repos/[repo].tsx
@@ -1,3 +1,4 @@
+import { Component, ComponentProps } from 'react';
 import type { GetStaticPaths, GetStaticProps } from 'next';
 import { readData, readReposIds } from '@/lib/repos';
 import type Language from '@/components/repos/lang_colors';
@@ -8,36 +9,44 @@ import RepoCard from '@/components/repos/card';
 import type Repository from '@/types/repos';
 import names from 'classnames';
 
-export default function Repo({ repository }: { repository: Repository }) {
-    const { full_name, html_url, languages } = repository;
-    const languageIter = Object.entries(languages) as [Language, number][];
-    const totalCount = languageIter.reduce((pre, cur) => pre + cur[1], 0);
-    return (
-        <article aria-label='repositories'>
-            <Link
-                href={html_url}
-                className={names(
-                    'box-border leading-tight break-words',
-                    'text-sm font-medium text-slate-300'
-                )}>
-                {full_name}
-            </Link>
-            <RepoCard repository={repository} />
-            <LanguageStateBar languages={languageIter} totalCount={totalCount} />
-            <ul className='p-0 mt-0 list-none'>
-                {languageIter.map(([lang, count], id) => {
-                    return (
-                        <LanguageButton
-                            language={lang}
-                            percent={(count / totalCount) * 100}
-                            key={id}
-                            index={id}
-                        />
-                    );
-                })}
-            </ul>
-        </article>
-    );
+type RepoProps = ComponentProps<'article'> & {
+    repository: Repository;
+};
+
+class Repo extends Component<RepoProps> {
+    render() {
+        const { repository } = this.props;
+        const { full_name, html_url, languages } = repository;
+        const languageIter = Object.entries(languages) as [Language, number][];
+        const totalCount = languageIter.reduce((pre, cur) => pre + cur[1], 0);
+
+        return (
+            <article aria-label='repositories'>
+                <Link
+                    href={html_url}
+                    className={names(
+                        'box-border leading-tight break-words',
+                        'text-sm font-medium text-slate-300'
+                    )}>
+                    {full_name}
+                </Link>
+                <RepoCard repository={repository} />
+                <LanguageStateBar languages={languageIter} totalCount={totalCount} />
+                <ul className='p-0 mt-0 list-none'>
+                    {languageIter.map(([lang, count], id) => {
+                        return (
+                            <LanguageButton
+                                language={lang}
+                                percent={(count / totalCount) * 100}
+                                key={id}
+                                index={id}
+                            />
+                        );
+                    })}
+                </ul>
+            </article>
+        );
+    }
 }
 
 export const getStaticPaths: GetStaticPaths = () => {
@@ -49,3 +58,5 @@ export const getStaticProps: GetStaticProps = ({ params }) => {
     const repository = readData(params?.repo as string);
     return { props: { repository } };
 };
+
+export default Repo;
--- pages/repos/index.tsx
+++ pages/repos/index.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from 'react';
-import type { GetStaticProps } from 'next';
+import React, { Component, ComponentProps, createRef } from 'react';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Layout from '@/components/layout';
 import ReactGitHubCalendar from '@/components/calendar';
@@ -9,43 +9,52 @@ import ResumeSection from '@/components/section';
 import { readRepositories } from '@/lib/repos';
 import { secondaryTitle } from '@/constants/';
 
-export default function Portfolio({ repositoryData }: { repositoryData: Repository[] }) {
-    const sectionRef = useRef<HTMLElement>(null);
-    useEffect(() => {
-        if (sectionRef.current) {
-            // 홈스크린과 프로필 부분이 같으니까 스킵하기
-            sectionRef.current?.firstElementChild?.scrollIntoView({
+type PortfolioProps = ComponentProps<'div'> & {
+    repositoryData: Repository[];
+};
+
+class Portfolio extends Component<PortfolioProps> {
+    sectionRef = createRef<HTMLElement>();
+
+    componentDidMount() {
+        if (this.sectionRef.current) {
+            this.sectionRef.current.firstElementChild?.scrollIntoView({
                 behavior: 'smooth',
                 block: 'start',
             });
         }
-    }, [sectionRef]);
+    }
+    render() {
+        const { repositoryData } = this.props;
 
-    return (
-        <Layout>
-            <Head>
-                <meta
-                    name='keywords'
-                    content='서버/백엔드, 웹 풀스택, 크로스플랫폼개발, 개발자 구인'
-                />
-                <title>{secondaryTitle}</title>
-            </Head>
-            <section
-                aria-label='repositories'
-                ref={sectionRef}
-                className='w-full p-6 mx-0 my-6 border border-gray-300 border-solid rounded-lg'>
-                {repositoryData.map((repo, id) => {
-                    return <Repo repository={repo} key={id} />;
-                })}
-            </section>
-            <ResumeSection type='contributions'>
-                <ReactGitHubCalendar />
-            </ResumeSection>
-        </Layout>
-    );
+        return (
+            <Layout>
+                <Head>
+                    <meta
+                        name='keywords'
+                        content='서버/백엔드, 웹 풀스택, 크로스플랫폼개발, 개발자 구인'
+                    />
+                    <title>{secondaryTitle}</title>
+                </Head>
+                <section
+                    aria-label='repositories'
+                    ref={this.sectionRef}
+                    className='w-full p-6 mx-0 my-6 border border-gray-300 border-solid rounded-lg'>
+                    {repositoryData.map((repo, id) => {
+                        return <Repo repository={repo} key={id} />;
+                    })}
+                </section>
+                <ResumeSection type='contributions'>
+                    <ReactGitHubCalendar />
+                </ResumeSection>
+            </Layout>
+        );
+    }
 }
 
 export const getStaticProps: GetStaticProps = () => {
     const repositoryData = readRepositories();
     return { props: { repositoryData } };
 };
+
+export default Portfolio;
