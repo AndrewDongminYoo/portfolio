32b1de501b9050f198891a37a93cd066988b53ea
Dongmin Yu <ydm2790@gmail.com>
2023-07-05 20:51:10 +0900

    perf(libs): 🎨 Trunk.io 린터/포맷터 업데이트 및 type declaration 파일들 무시 설정함(그냥.. 줄 맞춰놓음).

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,11 @@
 declare module '@/env' {
-    readonly export const NODE_ENV: 'development' | 'production' | 'test';
-    readonly export const GITHUB_TOKEN: string;
-    readonly export const NEXT_PUBLIC_AUTH0_DOMAIN: string;
-    readonly export const NEXT_PUBLIC_AUTH0_CLIENT_ID: string;
-    readonly export const NEXT_PUBLIC_AUTH0_ADMIN_EMAIL: string;
-    readonly export const NEXT_PUBLIC_BASE_URL: string;
-    readonly export const REDIS_URL: string;
-    readonly export const OPENAI_ORGANIZATION: string;
-    readonly export const OPENAI_API_KEY: string;
+    const NODE_ENV: 'development' | 'production' | 'test';
+    const GITHUB_TOKEN: string;
+    const NEXT_PUBLIC_AUTH0_DOMAIN: string;
+    const NEXT_PUBLIC_AUTH0_CLIENT_ID: string;
+    const NEXT_PUBLIC_AUTH0_ADMIN_EMAIL: string;
+    const NEXT_PUBLIC_BASE_URL: string;
+    const REDIS_URL: string;
+    const OPENAI_ORGANIZATION: string;
+    const OPENAI_API_KEY: string;
 }
