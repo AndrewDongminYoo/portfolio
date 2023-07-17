70fcd749650eaceeadc064521189a7a141790969
Dongmin Yu <ydm2790@gmail.com>
2023-07-18 02:09:52 +0900

    style(styles): 📱 스토리북 for Next.js 초기설정

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import * as path from 'path';
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+    stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {
+            nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+        },
+    },
+    docs: {
+        autodocs: 'tag',
+    },
+};
+
+export default config;
