fe1b0dd88a471bb9a01fd608d5139d995750f643
Dongmin Yu <ydm2790@gmail.com>
2023-07-18 01:11:47 +0900

    refactor(libs): ➖ 옵셔널/피어 디펜던시 모두 정리

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,7 +4,15 @@ import { Slot } from '@radix-ui/react-slot';
 import { names } from '@/lib/utils';
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+    names(
+        'inline-flex items-center justify-center',
+        'rounded-md text-sm font-medium transition-colors',
+        'focus-visible:outline-none',
+        'focus-visible:ring-1',
+        'focus-visible:ring-ring',
+        'disabled:pointer-events-none',
+        'disabled:opacity-50'
+    ),
     {
         variants: {
             variant: {
