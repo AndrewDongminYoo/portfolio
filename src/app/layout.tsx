8ef08ec86c364848aabde81851a2a042dc0840be
Dongmin Yu <ydm2790@gmail.com>
2026-01-06 15:21:34 +0900

    chore: adds Google Tag Manager
    Integrates Google Tag Manager (GTM) into the application layout. This enables centralized management of various tracking and marketing tags, enhancing flexibility and control over analytics and conversion tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -101,13 +101,32 @@ export default function RootLayout({
         <GoogleAnalytics gaId='G-954YXFWZGG' />
         {/* Vercel Speed Insight Script */}
         <SpeedInsights />
+        {/* Google Tag Manager */}
+        <Script id='clarity' type='text/javascript' strategy='afterInteractive'>
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K5JB5526');
+          `}
+        </Script>
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-K5JB5526'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {/* Microsoft Clarity Script */}
         <Script id='clarity' type='text/javascript' strategy='afterInteractive'>
           {`
             (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "ux1kntnn9n");
           `}
         </Script>
